9f8c727fbbbc8e7136122d16468013a033c2d6a1
Tormod Liseth <tormod@sifterstudios.no>
2021-05-09 23:39:37 +0200

    Added walk animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,5 +9,9 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal") * movementSpeed;
         var rb = GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(horizontal, rb.velocity.y);
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0;
+        animator.SetBool("Walk", walking);
     }
 }
