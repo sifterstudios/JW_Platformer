9f3c33232fe1c765c48b6a95ba155b79b69d233a
Tormod Liseth <tormod@sifterstudios.no>
2021-05-09 23:30:26 +0200

    Added Animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] float movementSpeed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var rigidbody2D = GetComponent<Rigidbody2D>();
-        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        var horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        var rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(horizontal, rb.velocity.y);
     }
 }
