b930cf64e7c43bde261e52ed8160714f4f7791e5
Tormod Liseth <tormod@sifterstudios.no>
2021-05-09 22:48:42 +0200

    Done with section 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+    }
+}
