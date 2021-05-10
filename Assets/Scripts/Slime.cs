571d2db69a9c1dc19e8361c36a34b363ff5282fe
Tormod Liseth <tormod@sifterstudios.no>
2021-05-11 00:22:18 +0200

    Slime now turns to not fall!

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] Transform leftSensor;
+    [SerializeField] Transform rightSensor;
     Rigidbody2D _rb;
+    float _direction = -1;
 
     void Start()
     {
@@ -14,13 +17,41 @@ public class Slime : MonoBehaviour
 
     void Update()
     {
-        _rb.velocity = Vector2.left;
+        _rb.velocity = new Vector2(_direction, _rb.velocity.y);
+
+        if (_direction < 0)
+        {
+            Debug.DrawRay(leftSensor.position, Vector2.down * 0.1f, Color.red);
+
+            var result = Physics2D.Raycast(leftSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+            {
+                TurnAround();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(rightSensor.position, Vector2.down * 0.1f, Color.red);
+
+            var result = Physics2D.Raycast(rightSensor.position, Vector2.down, 0.1f);
+            if (result.collider == null)
+            {
+                TurnAround();
+            }
+        }
+    }
+
+    void TurnAround()
+    {
+        _direction *= -1;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = _direction > 0;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         var player = other.collider.GetComponent<Player>();
-        if (player==null)
+        if (player == null)
             return;
 
         player.ResetToStart();
