7908e8454aff7fa3ea49c636b1c367226430d70f
Tormod Liseth <tormod@sifterstudios.no>
2021-05-12 00:46:30 +0200

    Implemented the wiggle functionality

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
+    Vector3 _initialPosition;
     readonly HashSet<Player> _playersInTrigger = new HashSet<Player>();
+
+    void Start()
+    {
+        _initialPosition = transform.position;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,8 +22,8 @@ public class FallingPlatform : MonoBehaviour
         _playersInTrigger.Add(player);
 
         PlayerInside = true;
-
-        StartCoroutine(WiggleAndFall());
+        if (_playersInTrigger.Count == 1)
+            StartCoroutine(WiggleAndFall());
     }
 
     void OnTriggerExit2D(Collider2D other)
@@ -26,7 +32,11 @@ public class FallingPlatform : MonoBehaviour
         if (player == null)
             return;
         _playersInTrigger.Remove(player);
-        if (_playersInTrigger.Count == 0) PlayerInside = false;
+        if (_playersInTrigger.Count == 0)
+        {
+            PlayerInside = false;
+            StopCoroutine(WiggleAndFall());
+        }
     }
 
     IEnumerator WiggleAndFall()
@@ -34,7 +44,18 @@ public class FallingPlatform : MonoBehaviour
         Debug.Log("Waiting to wiggle");
         yield return new WaitForSeconds(0.25f);
         Debug.Log("Wiggling");
-        yield return new WaitForSeconds(1f);
+        float wiggleTimer = 0;
+
+        while (wiggleTimer < 1f)
+        {
+            var randomX = Random.Range(-0.05f, 0.05f);
+            var randomY = Random.Range(-0.05f, 0.05f);
+            transform.position = _initialPosition + new Vector3(randomX, randomY);
+            var randomDelay = Random.Range(0.005f, 0.01f);
+            yield return null;
+            wiggleTimer += randomDelay;
+        }
+
         Debug.Log("Falling");
         yield return new WaitForSeconds(3f);
     }
