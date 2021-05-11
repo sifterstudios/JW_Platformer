93921dbd4eb258ea83da9ce789f05432ff2aca00
Tormod Liseth <tormod@sifterstudios.no>
2021-05-12 00:26:17 +0200

    Added coroutine to falling platforms

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +5,37 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     public bool PlayerInside;
+    readonly HashSet<Player> _playersInTrigger = new HashSet<Player>();
 
     void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
-        if (player==null)
+        if (player == null)
             return;
 
+        _playersInTrigger.Add(player);
+
         PlayerInside = true;
+
+        StartCoroutine(WiggleAndFall());
     }
+
     void OnTriggerExit2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
-        if (player==null)
+        if (player == null)
             return;
+        _playersInTrigger.Remove(player);
+        if (_playersInTrigger.Count == 0) PlayerInside = false;
+    }
 
-        PlayerInside = false;
+    IEnumerator WiggleAndFall()
+    {
+        Debug.Log("Waiting to wiggle");
+        yield return new WaitForSeconds(0.25f);
+        Debug.Log("Wiggling");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Falling");
+        yield return new WaitForSeconds(3f);
     }
 }
