4806dbcd00a3dabef00d7c55b61719c42a434dbe
Tormod Liseth <tormod@sifterstudios.no>
2021-05-11 14:20:06 +0200

    Added jump animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -148,6 +148,7 @@ public class Player : MonoBehaviour
     {
         bool walking = _horizontal != 0;
         _animator.SetBool("Walk", walking);
+        _animator.SetBool("Jump", ShouldContinueJump());
     }
 
     void UpdateIsGrounded()
