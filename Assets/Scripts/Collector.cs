5c9aaef5695fc8bfcfee537da355c18844df7fc2
Tormod Liseth <tormod@sifterstudios.no>
2021-05-13 00:31:52 +0200

    Added UI elements to colllector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,14 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
+    TMP_Text _remainingText;
+
+    void Start()
+    {
+        _remainingText = GetComponentInChildren<TMP_Text>();
+    }
 
     void Update()
     {
+        var countRemaining = 0;
         foreach (var collectible in _collectibles)
             if (collectible.isActiveAndEnabled)
-                return;
+                countRemaining++;
+        _remainingText?.SetText(countRemaining.ToString());
+        if (countRemaining > 0)
+            return;
+
         print("Got All Gems");
     }
 }
