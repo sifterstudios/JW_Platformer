f81951f50728b2c70a05687117508fabee9e235f
Tormod Liseth <tormod@sifterstudios.no>
2021-05-12 15:09:11 +0200

    Fireball collision system that kills..!

--- Assets/Scripts/KillOnEnter.cs
+++ Assets/Scripts/KillOnEnter.cs
@@ -2,13 +2,15 @@
 
 public class KillOnEnter : MonoBehaviour
 {
+    void OnParticleCollision(GameObject other)
+    {
+        var player = other.GetComponent<Player>();
+        if (player != null) player.ResetToStart();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
-        if (player != null)
-        {
-            player.ResetToStart();
-        }
-
+        if (player != null) player.ResetToStart();
     }
 }
