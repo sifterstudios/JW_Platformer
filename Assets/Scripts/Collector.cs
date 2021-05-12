defb3f20f10cceda9177d0eaf08c31b9e2fc854f
Tormod Liseth <tormod@sifterstudios.no>
2021-05-12 23:36:36 +0200

    Cleaned up some allocations (Garbage Collection)

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
+
+    void Update()
+    {
+        foreach (var collectible in _collectibles)
+            if (collectible.isActiveAndEnabled)
+                return;
+        print("Got All Gems");
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,34 +2,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] int playerNumber = 1;
-    [Header("Movement")] [SerializeField] float movementSpeed = 5f;
-    [SerializeField] float slipFactor = 1f;
-    [SerializeField] float airMovementSpeed = 1.2f;
-    [Header("Jump")] [SerializeField] float jumpVelocity = 500f;
-    [SerializeField] int maxJumps = 2;
-    [SerializeField] Transform feet;
-    [SerializeField] float downpull = 5f;
-    [SerializeField] float maxJumpDuration = 0.1f;
-
-
-    Vector3 _startPosition;
-    int _jumpsRemaining;
-    float _falltimer;
-    float _jumpTimer;
-    Rigidbody2D _rb;
+    [SerializeField] int _playerNumber = 1;
+    [Header("Movement")] [SerializeField] float _movementSpeed = 5f;
+    [SerializeField] float _slipFactor = 1f;
+    [SerializeField] float _airMovementSpeed = 1.2f;
+    [Header("Jump")] [SerializeField] float _jumpVelocity = 500f;
+    [SerializeField] int _maxJumps = 2;
+    [SerializeField] Transform _feet;
+    [SerializeField] float _downpull = 5f;
+    [SerializeField] float _maxJumpDuration = 0.1f;
     Animator _animator;
-    SpriteRenderer _spriteRenderer;
-    float _horizontal;
-    bool _isGrounded;
     Collider2D _colliderHit;
+    float _falltimer;
+    float _horizontal;
+    string _horizontalAxis;
+    bool _isGrounded;
 
-    public int PlayerNumber => playerNumber;
+    string _jumpButton;
+    int _jumpsRemaining;
+    float _jumpTimer;
+    int _layerMask;
+    Rigidbody2D _rb;
+    SpriteRenderer _spriteRenderer;
+    Vector3 _startPosition;
+
+    public int PlayerNumber => _playerNumber;
 
     void Start()
     {
+        _jumpButton = $"P{_playerNumber}Jump";
+        _horizontalAxis = $"P{_playerNumber}Horizontal";
+        _layerMask = LayerMask.GetMask("Default");
+
         _startPosition = transform.position;
-        _jumpsRemaining = maxJumps;
+        _jumpsRemaining = _maxJumps;
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
@@ -57,30 +63,30 @@ public class Player : MonoBehaviour
         if (_isGrounded && _falltimer > 0)
         {
             _falltimer = 0;
-            _jumpsRemaining = maxJumps;
+            _jumpsRemaining = _maxJumps;
         }
         else
         {
             _falltimer += Time.deltaTime;
-            var downForce = downpull * _falltimer * _falltimer;
+            var downForce = _downpull * _falltimer * _falltimer;
             _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y - downForce);
         }
     }
 
     void ContinueJump()
     {
-        _rb.velocity = (new Vector2(_rb.velocity.x, jumpVelocity));
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpVelocity);
         _falltimer = 0;
     }
 
     bool ShouldContinueJump()
     {
-        return Input.GetButton($"P{playerNumber}Jump") && _jumpTimer <= maxJumpDuration;
+        return Input.GetButton(_jumpButton) && _jumpTimer <= _maxJumpDuration;
     }
 
     void Jump()
     {
-        _rb.velocity = new Vector2(_rb.velocity.x, jumpVelocity);
+        _rb.velocity = new Vector2(_rb.velocity.x, _jumpVelocity);
         _jumpsRemaining--;
         _falltimer = 0;
         _jumpTimer = 0;
@@ -88,7 +94,7 @@ public class Player : MonoBehaviour
 
     bool ShouldStartJump()
     {
-        return Input.GetButtonDown($"P{playerNumber}Jump") && _jumpsRemaining > 0;
+        return Input.GetButtonDown(_jumpButton) && _jumpsRemaining > 0;
     }
 
     void CheckGroundType()
@@ -96,13 +102,9 @@ public class Player : MonoBehaviour
         if (_colliderHit != null)
         {
             if (_colliderHit.CompareTag("Slippery"))
-            {
                 SlipHorizontal();
-            }
             else
-            {
                 MoveHorizontal();
-            }
         }
         else
         {
@@ -112,48 +114,45 @@ public class Player : MonoBehaviour
 
     void MoveInAir()
     {
-        _rb.velocity = new Vector2(_horizontal * airMovementSpeed, _rb.velocity.y);
+        _rb.velocity = new Vector2(_horizontal * _airMovementSpeed, _rb.velocity.y);
     }
 
     void MoveHorizontal()
     {
-        _rb.velocity = new Vector2(_horizontal * movementSpeed, _rb.velocity.y);
+        _rb.velocity = new Vector2(_horizontal * _movementSpeed, _rb.velocity.y);
     }
 
     void SlipHorizontal()
     {
-        var desiredVelocity = new Vector2(_horizontal * movementSpeed, _rb.velocity.y);
+        var desiredVelocity = new Vector2(_horizontal * _movementSpeed, _rb.velocity.y);
         var smoothedVelocity = Vector2.Lerp(
             _rb.velocity,
             desiredVelocity,
-            Time.deltaTime / slipFactor);
+            Time.deltaTime / _slipFactor);
 
         _rb.velocity = smoothedVelocity;
     }
 
     void ReadHorizontalInput()
     {
-        _horizontal = Input.GetAxis($"P{playerNumber}Horizontal") * movementSpeed;
+        _horizontal = Input.GetAxis(_horizontalAxis) * _movementSpeed;
     }
 
     void UpdateSpriteDirection()
     {
-        if (_horizontal != 0)
-        {
-            _spriteRenderer.flipX = _horizontal < 0;
-        }
+        if (_horizontal != 0) _spriteRenderer.flipX = _horizontal < 0;
     }
 
     void UpdateAnimator()
     {
-        bool walking = _horizontal != 0;
+        var walking = _horizontal != 0;
         _animator.SetBool("Walk", walking);
         _animator.SetBool("Jump", ShouldContinueJump());
     }
 
     void UpdateIsGrounded()
     {
-        _colliderHit = Physics2D.OverlapCircle(feet.position, 0.1f, LayerMask.GetMask("Default"));
+        _colliderHit = Physics2D.OverlapCircle(_feet.position, 0.1f, _layerMask);
         _isGrounded = _colliderHit != null;
     }
 
