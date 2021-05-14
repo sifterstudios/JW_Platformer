c25710016b26ebdee520dd6627341f8745531dc0
Tormod Liseth <tormod@sifterstudios.no>
2021-05-14 17:43:43 +0200

    Made ItemBox implement our base class

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -4,36 +4,25 @@ public class ItemBox : HittableFromBelow
 {
     [SerializeField] GameObject _item;
     [SerializeField] Vector2 _itemLaunchVelocity;
-    SpriteRenderer _spriteRenderer;
     bool _used;
+
+    protected override bool CanUse => !_used && _item != null;
 
     void Start()
     {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         if (_item != null)
             _item.SetActive(false);
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    protected override void Use()
     {
-        if (_used)
+        if (_item == null)
             return;
-
-        var player = other.collider.GetComponent<Player>();
-        if (player == null)
-            return;
-
-        if (other.contacts[0].normal.y > 0)
-        {
-            _spriteRenderer.sprite = _usedSprite;
-            if (_item != null)
-            {
-                _used = true;
-                _item.SetActive(true);
-                var itemRigidbody = _item.GetComponent<Rigidbody2D>();
-                if (itemRigidbody != null)
-                    itemRigidbody.velocity = _itemLaunchVelocity;
-            }
-        }
+        base.Use();
+        _used = true;
+        _item.SetActive(true);
+        var itemRigidbody = _item.GetComponent<Rigidbody2D>();
+        if (itemRigidbody != null)
+            itemRigidbody.velocity = _itemLaunchVelocity;
     }
 }
