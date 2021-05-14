39ff11166647188bdc0a3cffcfd536ce377e93c8
Tormod Liseth <tormod@sifterstudios.no>
2021-05-14 02:21:50 +0200

    Removed all garbace allocation! :D

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,11 +2,19 @@
 
 public class Collectible : MonoBehaviour
 {
+    Collector _collector;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
         if (player == null)
             return;
         gameObject.SetActive(false);
+        _collector.ItemPickedUp();
+    }
+
+    public void SetCollector(Collector collector)
+    {
+        _collector = collector;
     }
 }
--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -8,28 +8,33 @@ public class Collector : MonoBehaviour
 {
     [SerializeField] List<Collectible> _collectibles;
     [SerializeField] UnityEvent _onCollectionComplete;
+    int _countCollected;
     TMP_Text _remainingText;
 
     void Start()
     {
         _remainingText = GetComponentInChildren<TMP_Text>();
-    }
 
-    void Update()
-    {
-        var countRemaining = 0;
-        foreach (var collectible in _collectibles)
-            if (collectible.isActiveAndEnabled)
-                countRemaining++;
+        foreach (var collectible in _collectibles) collectible.SetCollector(this);
+
+        var countRemaining = _collectibles.Count - _countCollected;
         _remainingText?.SetText(countRemaining.ToString());
-        if (countRemaining > 0)
-            return;
-
-        _onCollectionComplete.Invoke();
     }
 
     void OnValidate()
     {
         _collectibles = _collectibles.Distinct().ToList();
+    }
+
+    public void ItemPickedUp()
+    {
+        _countCollected++;
+        var countRemaining = _collectibles.Count - _countCollected;
+
+        _remainingText?.SetText(countRemaining.ToString());
+        if (countRemaining > 0)
+            return;
+
+        _onCollectionComplete.Invoke();
     }
 }
