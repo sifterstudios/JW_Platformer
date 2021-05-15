de1df1deb0c685701c6e7501bcf3e0de2cfd3057
Tormod Liseth <tormod@sifterstudios.no>
2021-05-15 03:41:32 +0200

    Working visual toggle switch

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -12,13 +12,21 @@ public class ToggleSwitch : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         var player = other.GetComponent<Player>();
         if (player == null)
             return;
+        var playerRigidbody = player.GetComponent<Rigidbody2D>();
+        if (playerRigidbody == null)
+            return;
         var wasOnRight = other.transform.position.x > transform.position.x;
+        var playerWalkingRight = playerRigidbody.velocity.x > 0;
+        var playerWalkingLeft = playerRigidbody.velocity.x < 0;
 
-        _spriteRenderer.sprite = wasOnRight ? _leftSprite : _rightSprite;
+        if (wasOnRight && playerWalkingRight)
+            _spriteRenderer.sprite = _rightSprite;
+        else if (!wasOnRight && !playerWalkingLeft)
+            _spriteRenderer.sprite = _leftSprite;
     }
 }
