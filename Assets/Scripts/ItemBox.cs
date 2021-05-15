6ce23f10fa6d93c85c61e5ac7db2e8bc74eac159
Tormod Liseth <tormod@sifterstudios.no>
2021-05-15 07:34:08 +0200

    Made it so that our itembox can spawn prefabs!

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class ItemBox : HittableFromBelow
 {
     [SerializeField] GameObject _item;
+    [SerializeField] GameObject _itemPrefab;
     [SerializeField] Vector2 _itemLaunchVelocity;
+
     bool _used;
 
-    protected override bool CanUse => !_used && _item != null;
+    protected override bool CanUse => !_used;
 
     void Start()
     {
@@ -16,9 +18,16 @@ public class ItemBox : HittableFromBelow
 
     protected override void Use()
     {
+        _item = Instantiate(_itemPrefab,
+            transform.position + Vector3.up,
+            Quaternion.identity,
+            transform);
+
         if (_item == null)
             return;
+
         base.Use();
+
         _used = true;
         _item.SetActive(true);
         var itemRigidbody = _item.GetComponent<Rigidbody2D>();
